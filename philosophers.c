479878dcf194c3847e6aa1a589de2c26f74968a2
marcor2742 <marcoruggiero.mail@gmail.com>
2024-01-17 18:40:45 +0100

    base thread e mutex

--- philosophers.c
+++ philosophers.c
@@ -1,16 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosopher.c                                      :+:      :+:    :+:   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mruggier <mruggier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 15:21:56 by mruggier          #+#    #+#             */
-/*   Updated: 2024/01/17 15:22:00 by mruggier         ###   ########.fr       */
+/*   Updated: 2024/01/17 18:17:40 by mruggier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philosophers.h"
+
+void *do_philosophy(void *arg)
+{
+	t_lock *lock = (t_lock *)arg;
+	pthread_mutex_lock(&lock->lock);
+	lock->ledger += 1;
+	pthread_mutex_unlock(&lock->lock);
+	return (NULL);
+}
+
 int main(int argc, char **argv)
 {
+	t_lock lock;
+	pthread_t	thread[100]; // array of threads forks
+	lock.ledger = 0;
+	int i;
+	
+	pthread_mutex_init(&lock.lock, NULL);
+	i = 0;
+
+	while (i < 100)
+	{
+		pthread_create(thread + i, NULL, do_philosophy, (void *)&lock); 
+		
+		i++;
+	}
+	i = 0;
+	while (i < 100)
+	{
+		pthread_join(thread[i], NULL); // wait for thread to finish
+		
+		i++;
+	}
+	printf ("ledger: %d\n", lock.ledger);
+	pthread_mutex_destroy(&lock.lock);
 	return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosopher.h                                      :+:      :+:    :+:   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mruggier <mruggier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 15:21:31 by mruggier          #+#    #+#             */
-/*   Updated: 2024/01/17 15:21:50 by mruggier         ###   ########.fr       */
+/*   Updated: 2024/01/17 18:13:20 by mruggier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 # define PHILOSOPHER_H
 
 # include <pthread.h>
+#include <stdio.h>
+
+typedef struct	s_lock
+{
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	lock;
+	int ledger;
+}				t_lock;
 
 #endif
